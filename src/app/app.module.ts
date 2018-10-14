0ae2d55ec54882c86eb97fd34a2c2196d98ac889
Alberto Durand <adurandt@gmail.com>
2018-10-13 21:18:34 -0500

    Modificaciones en los módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,35 +4,28 @@ import { NgModule } from '@angular/core';
 // Routes
 import { APP_ROUTES } from './app.routes';
 
+// Modules
+import { PagesModule } from './pages/pages.module';
+
+// Components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent,
-    ProgressComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumsComponent,
-    PagenotfoundComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
